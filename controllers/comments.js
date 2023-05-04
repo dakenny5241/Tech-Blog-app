c43a81eaf9a5b00eb47c3921b6e5e9ca828d1b37
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-05-04 07:04:06 +0100

    controllers comments setup

---
+++ controllers/comments.js
@@ -0,0 +1,39 @@
+const Comment = require('../models/comment');
+
+exports.create = (req, res) => {
+  if (req.session.user) {
+    Comment.create({
+      content: req.body.content,
+      postId: req.params.id
+    }).then(() => {
+      res.redirect(`/post/${req.params.id}`);
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+```
+
+**models/post.js**
+```js
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('post', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  });
+
+  return Post;
+};
+```
+
+**models/comment.js**
+```js
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('comment', {
+    content: DataTypes.TEXT,
+    postId: DataTypes.INTEGER
+  });
+
+  return Comment;
+};
+```
